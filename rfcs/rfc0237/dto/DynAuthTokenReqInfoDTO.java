a874c657337d7b85d07ea89fda700f1c2a549437
Hiroyuki Maeomichi <maeomichi.hiroyuki@lab.ntt.co.jp>
2020-03-18 17:42:39 +0900

    delete miscellaneous files

--- rfcs/rfc0237/dto/DynAuthTokenReqInfoDTO.java
+++
@@ -1,6 +0,0 @@
-package org.osgi.service.onem2m.dto;
-import java.util.*;
-public class DynAuthTokenReqInfoDTO extends org.osgi.dto.DTO{
-	@javax.xml.bind.annotation.XmlElement( required  = true)
-	public List<Map> dasInfo;//Map : DasInfo
-}
--- rfcs/rfc0237/dto/ResourceWrapperDTO.java
+++
@@ -1,7 +0,0 @@
-package org.osgi.service.onem2m.dto;
-public class ResourceWrapperDTO extends org.osgi.dto.DTO{
-	@javax.xml.bind.annotation.XmlElement( required  = true)
-	public String uri;
-	
-	ResourceDTO resource;
-}
